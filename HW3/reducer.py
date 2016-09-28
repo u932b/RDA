3efdf7574ab3a448a812b7556fff06d3ddce1c01
za0 <za0@gmx.com>
2016-09-27 21:52:10 -0400

    Rewrite reducer.

--- HW3/reducer.py
+++ HW3/reducer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import sys
+import collections
 
 current_key = None
 current_value = []
 current_PR = 0
 word = None
-real_value = None
+last_value = None
+answer_dict = collections.OrderedDict()
 
 for line in sys.stdin:
     line = line.strip()
@@ -17,37 +19,20 @@ for line in sys.stdin:
         key = key.split('=')[1]
         value = value.split('=')[1]
         PR = float(PR)
+        if key not in answer_dict:
+            answer_dict[key] = [None, PR]
+        else:
+            answer_dict[key][1] += PR
     else:
         # one's without PR
         key, value = line_array
         key = key.split('=')[1]
         value = value.split('=')[1]
-        if current_value:
-            # already a value inside
-            # print current_key, current_value
-            real_value = current_value.pop()
-        current_value.append(value)
-        # print current_key, current_value
         PR = 0
-    if current_key == key:
-        current_PR += PR
-    else:
-        if current_key:
-            if real_value:
-                print '%s %s %f' % (current_key, real_value, current_PR)
-                real_value = None
-                current_value.pop()
-            else:
-                try:
-                    print '%s %s %f' % (current_key, current_value.pop(), current_PR)
-                except:
-                    pass
-        current_key = key
-        current_PR = PR
-        if PR == 0:
-            current_value.append(value)
-try:
-    if current_key == key:
-        print '%s %s %f' % (current_key, current_value.pop(), current_PR)
-except:
-    pass
+        if key not in answer_dict:
+            answer_dict[key] = [value, PR]
+        else:
+            answer_dict[key][0] = value
+
+for item in answer_dict.items():
+    print '%s %s %f' % (item[0], item[1][0], item[1][1])
