82a74b107508a7eed20ce441ede01e4613aa7875
za0 <za0@gmx.com>
2016-09-28 16:17:09 -0400

    Rename reducers.

---
+++ HW3/reducer-twoforloop.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import collections
+
+pg_dict = {}
+answer_dict = collections.OrderedDict()
+
+for line in sys.stdin:
+    line = line.strip()
+    line_array = line.split(', ')
+    if len(line_array) > 2:
+        # ordinary ones
+        key, value, PR = line_array
+        key = key.split('=')[1]
+        PR = float(PR)
+        if key not in answer_dict:
+            if key not in pg_dict:
+                pg_dict[key] = PR
+                print 'Creating pg_dict: ', key, pg_dict[key]
+            else:
+                pg_dict[key] += PR
+                print 'Appending to pg_dict:', key, pg_dict[key]
+        else:
+            answer_dict[key][1] += PR
+    else:
+        # one's without PR
+        key, value = line_array
+        key = key.split('=')[1]
+        value = value.split('=')[1]
+        if key not in answer_dict:
+            if key in pg_dict:
+                answer_dict[key] = [value, pg_dict[key]]
+            else:
+                answer_dict[key] = [value, 0]
+        else:
+            raise Exception('I know Python!')
+
+for item in answer_dict.items():
+    print '%s %s %f' % (item[0], item[1][0], item[1][1])
--- HW3/reducer.py
+++ HW3/reducer.py
@@ -1,40 +1,55 @@
 #!/usr/bin/env python
 
 import sys
-import collections
+import re
 
-pg_dict = {}
-answer_dict = collections.OrderedDict()
+current_key = None
+current_value = []
+current_PR = 0
+word = None
+last_value = None
 
 for line in sys.stdin:
     line = line.strip()
-    line_array = line.split(', ')
+    line_array = re.split('\t|,\ ', line)
     if len(line_array) > 2:
         # ordinary ones
         key, value, PR = line_array
         key = key.split('=')[1]
+        value = value.split('=')[1]
         PR = float(PR)
-        if key not in answer_dict:
-            if key not in pg_dict:
-                pg_dict[key] = PR
-                print 'Creating pg_dict: ', key, pg_dict[key]
-            else:
-                pg_dict[key] += PR
-                print 'Appending to pg_dict:', key, pg_dict[key]
-        else:
-            answer_dict[key][1] += PR
     else:
         # one's without PR
         key, value = line_array
         key = key.split('=')[1]
         value = value.split('=')[1]
-        if key not in answer_dict:
-            if key in pg_dict:
-                answer_dict[key] = [value, pg_dict[key]]
+        if current_value:
+            # already a value inside
+            # print current_key, current_value
+            last_value = current_value.pop()
+        current_value.append(value)
+        # print current_key, current_value
+        PR = 0
+    if current_key == key:
+        current_PR += PR
+    else:
+        if current_key:
+            if last_value:
+                print '%s %s %f' % (current_key, last_value, current_PR)
+                last_value = None
+                current_value.pop()
             else:
-                answer_dict[key] = [value, 0]
-        else:
-            raise Exception('I know Python!')
-
-for item in answer_dict.items():
-    print '%s %s %f' % (item[0], item[1][0], item[1][1])
+                try:
+                    print '%s %s %f' % (current_key, current_value.pop(),
+                                        current_PR)
+                except:
+                    pass
+        current_key = key
+        current_PR = PR
+        if PR == 0:
+            current_value.append(value)
+if current_key == key:
+    try:
+        print '%s %s %f' % (current_key, current_value.pop(), current_PR)
+    except:
+        print '%s %s %f' % (current_key, value, current_PR)
