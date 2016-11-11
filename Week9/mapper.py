3684d0debf8c16a16c229d637d135287111ab551
za0 <za0@gmx.com>
2016-11-11 18:28:54 -0500

    Draft for HW9.

--- Week9/mapper.py
+++ Week9/mapper.py
@@ -3,16 +3,22 @@
 import sys
 
 result_list = []
-output_format = {'key': None, 'value': [], 'PR': None}
+# output_format = {'key': None, 'value': [], 'PR': None}
+header = """id,station_id,status,available_bike_count,available_dock_count,created_at,station_summary_id
+"""
+header_list = header.rstrip().lstrip().split(',')
+# print header_list
 for line in sys.stdin:
-    list = line.split('\t')
-    # line = line.strip()
-    # parse_line = line.split()
-
+    line = line.strip()
+    parse_line = line.split(',')
+    if parse_line[1] == header_list[1]:
+        continue
+    dic = dict(zip(header_list, parse_line))
     # output_format['key'] = parse_line[0]
     # output_format['value'] = parse_line[1:-1]
     # output_format['PR'] = None
-    # print "key=%s\tvalue=%s" % (parse_line[0], ' '.join(parse_line[1:-1]))
+    for key in dic.keys():
+        print "key=%s\tvalue=%s" % (key, dic[key])
 
     # output_format['value'] = [parse_line[0]]
     # PR = float(parse_line[-1])
@@ -21,4 +27,4 @@ for line in sys.stdin:
     #     output_format['PR'] = PR / len(parse_line[1:-1])
     #     print "key=%s\tvalue=%s, %f" % (key, parse_line[0], PR /
     #                                     len(parse_line[1:-1]))
-    print list
+    # print dic
--- Week9/reducer.py
+++ Week9/reducer.py
@@ -4,46 +4,63 @@ import sys
 import re
 
 current_key = None
-current_value = []
-current_PR = 0
+current_max = -1
+current_min = -1
+max_value = 0
+min_value = 0
 word = None
 last_value = None
+# max_value = -1
+# min_value = -1
 
 for line in sys.stdin:
+    # print line
     line = line.strip()
     line_array = re.split('\t|,\ ', line)
-    if len(line_array) > 2:
-        # ordinary ones
-        key, value, PR = line_array
-        key = key.split('=')[1]
-        value = value.split('=')[1]
-        PR = float(PR)
+    # print line_array
+    # if len(line_array) > 2:
+    #     # ordinary ones
+    key, value = line_array
+    key = key.split('=')[1]
+    value = value.split('=')[1]
+    global current_min
+    global current_max
+    if value.isdigit():
+        if current_min == -1:
+            current_min = value
+        if current_max == -1:
+            current_max = value
     else:
-        # one's without PR
-        key, value = line_array
-        key = key.split('=')[1]
-        value = value.split('=')[1]
-        if current_value:
-            # already a value inside
-            # print current_key, current_value
-            last_value = current_value.pop()
-        current_value.append(value)
-        # print current_key, current_value
-        PR = 0
+        value = len(value)
+        if current_min == -1:
+            current_min = value
+        if current_max == -1:
+            current_max = value
+    # else:
+    #     # one's without PR
+    #     key, value = line_array
+    #     key = key.split('=')[1]
+    #     value = value.split('=')[1]
+    #     if current_value:
+    #         # already a value inside
+    #         # print current_key, current_value
+    #         last_value = current_value.pop()
+    #     current_value.append(value)
+    #     # print current_key, current_value
+    #     PR = 0
+
     if current_key == key:
-        current_PR += PR
+        global max_value
+        global min_value
+        max_value = max(current_max, value)
+        min_value = min(current_min, value)
     else:
-        if current_key:
-            if last_value:
-                print '%s %s %f' % (current_key, last_value, current_PR)
-                last_value = None
-                current_value.pop()
-            else:
-                print '%s %s %f' % (current_key, current_value.pop(),
-                                    current_PR)
+        print '%s %s ' % (current_key, (max_value, min_value))
         current_key = key
-        current_PR = PR
-        if PR == 0:
-            current_value.append(value)
+        current_max = -1
+        current_min = -1
+    #     current_PR = PR
+    #     if PR == 0:
 if current_key == key:
-    print '%s %s %f' % (current_key, current_value.pop(), current_PR)
+    print 'last_shit:', current_key
+    print '%s %s ' % (current_key, (current_max, current_min))
